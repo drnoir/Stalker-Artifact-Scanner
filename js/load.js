423ff252151456dff707c3e986f24a13026a26ee
drnoir <chrispgodber.com>
2019-01-31 00:43:11 +0000

    final layout changes - added current time to header
    final layour changes
    added timestamp to header
    various css mods  and image swaps

--- js/load.js
+++ js/load.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 
+let now = new Date().toLocaleTimeString();
+
 // fade in the interface slowly
 window.onload = function(){
     $( ".bg-interface" ).animate({
         opacity: 1
       }, 1000, function() {
         $("#boot")[0].play();
+        $( "span.time" ).append(now);
   });
-};  
-});
+
+
+}});
