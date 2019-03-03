4ca58d8fac3722c45c3167a8efab3254a1226262
drnoir <chrispgodber.com>
2019-03-03 16:24:14 +0000

    updated jQuery to append instead of adding new CSS

--- js/clickevents.js
+++ js/clickevents.js
@@ -3,17 +3,21 @@
 $(document).ready(function(){
 
   $(".image-dot").click(function(){
-    $(".infobox2").hide();
-    $("#open")[0].play();
-    $(".infobox1").show();
-    console.log("image dot 1 clicked");
+    $("p.infobox").empty();
+    // $(".infobox2").hide();
+    // $("#open")[0].play();
+    // $(".infobox1").show();
+    // console.log("image dot 1 clicked");
+    $("p.infobox").append("This is the fuselage of the toxic waste ....")
   });
 
 
   $(".image-dot2").click(function(){
-    $(".infobox1").hide();
-    $("#open")[0].play();
-    $(".infobox2").show();
+    $("p.infobox").empty();
+    // $(".infobox1").hide();
+    // $("#open")[0].play();
+    // $(".infobox2").show();
+    $("p.infobox").append("This is blablablllba ....")
     console.log("image dot 2 clicked");
 });
 
